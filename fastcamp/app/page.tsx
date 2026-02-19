aa83b3722258df54e98d11c74796d14b30e94cbc
natthaphong67 <natthaphong5171@gmail.com>
2026-02-19 22:10:30 +0700

    test

--- fastcamp/app/page.tsx
+++ fastcamp/app/page.tsx
@@ -2,12 +2,13 @@ import Home from "./Page/(main)/Home/page"
 import Login from "./Page/(auth)/Login/page"
 import Register from "./Page/(auth)/Register/page"
 import Category from "./Page/(main)/Category/page"
+import Infomation from "./Page/(main)/Camp infomation/page"
 
 
 const Home_Page = () => {
   return (
     <div>
-      <Home/>
+      <Infomation />
     </div>
   )
 }
