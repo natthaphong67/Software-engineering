bbd8cba29566a70005962293a740efb1bcd98ac0
natthaphong67 <natthaphong5171@gmail.com>
2026-02-19 22:58:07 +0700

    #

--- fastcamp/app/Page/(main)/Camp infomation/page.tsx
+++ fastcamp/app/Page/(main)/Camp infomation/page.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Infomation = () => {
   return (
-    <div>
-        
+    <div className="w-full">
+        {/* Hero Section */}
+        <section className="relative w-full h-screen overflow-hidden bg-black">
+            <Image src="/Ai Innovation Summit 2025.png" alt="Ai Innovation Summit 2025" fill className="object-contain"/>
+        </section>
+
+
+        {/* Content Section */}
+        <section className="bg-white px-6 py-16">
+            <div className="mx-auto max-w-7xl rounded-4xl border p-8">
+            <h1 className="text-2xl font-bold mb-4">Descriptions</h1>
+            <p className="leading-relaxed text-gray-700">
+                วันที่ 12 ธันวาคม 2568 สถาบัน AI Engineering Institute (AIEI) ร่วมกับมหาวิทยาลัย CMKL
+                เชิญคุณเข้าร่วมงาน AI Innovation Summit 2025 ณ Club Siam Glowfish กรุงเทพฯ ...
+            </p>
+            </div>
+        </section>
     </div>
+
   )
 }
 
