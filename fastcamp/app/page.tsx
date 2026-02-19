c266b8b7452b8e66337c7a256ffc8e5d62ba1fe8
Dominixzy <khuan889@gmail.com>
2026-02-19 22:51:41 +0700

    redirect page

--- fastcamp/app/page.tsx
+++ fastcamp/app/page.tsx
@@ -1,14 +1,5 @@
-import Home from "./Page/(main)/Home/page"
-import Login from "./Page/(auth)/Login/page"
-import Register from "./Page/(auth)/Register/page"
-import Category from "./Page/(main)/Category/page"
+import { redirect } from "next/navigation";
 
-
-const Home_Page = () => {
-  return (
-    <div>
-      <Home/>
-    </div>
-  )
+export default function RootPage() {
+  redirect("/Page/Home");
 }
-export default Home_Page
